f91b8cc5d2dfe8d1691d9005a24dd495d0fef48e
TimAndy <xuchonglei@126.com>
2023-07-20 12:54:32 +0800

    Refactor the urlRewrite CloudFront function

--- functions/url-rewrite/index.js
+++ functions/url-rewrite/index.js
@@ -1,81 +1,122 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-function handler(event) {
-    var request = event.request;
-    var originalImagePath = request.uri;
-    //  validate, process and normalize the requested operations in query parameters
-    var normalizedOperations = {};
-    if (request.querystring) {
-        Object.keys(request.querystring).forEach(operation => {
-            switch (operation.toLowerCase()) {
-                case 'format':
-                    var SUPPORTED_FORMATS = ['auto', 'jpeg', 'webp', 'avif', 'png', 'svg', 'gif'];
-                    if (request.querystring[operation]['value'] && SUPPORTED_FORMATS.includes(request.querystring[operation]['value'].toLowerCase())) {
-                        var format = request.querystring[operation]['value'].toLowerCase(); // normalize to lowercase
-                        if (format === 'auto') {
-                            format = 'jpeg';
-                            if (request.headers['accept']) {
-                                if (request.headers['accept'].value.includes("avif")) {
-                                    format = 'avif';
-                                } else if (request.headers['accept'].value.includes("webp")) {
-                                    format = 'webp';
-                                }
-                            }
-                        }
-                        normalizedOperations['format'] = format;
-                    }
-                    break;
-                case 'width':
-                    if (request.querystring[operation]['value']) {
-                        var width = parseInt(request.querystring[operation]['value']);
-                        if (!isNaN(width) && (width > 0)) {
-                            // you can protect the Lambda function by setting a max value, e.g. if (width > 4000) width = 4000;
-                            normalizedOperations['width'] = width.toString();
-                        }
-                    }
-                    break;
-                case 'height':
-                    if (request.querystring[operation]['value']) {
-                        var height = parseInt(request.querystring[operation]['value']);
-                        if (!isNaN(height) && (height > 0)) {
-                            // you can protect the Lambda function by setting a max value, e.g. if (height > 4000) height = 4000;
-                            normalizedOperations['height'] = height.toString();
-                        }
-                    }
-                    break;
-                case 'quality':
-                    if (request.querystring[operation]['value']) {
-                        var quality = parseInt(request.querystring[operation]['value']);
-                        if (!isNaN(quality) && (quality > 0)) {
-                            if (quality > 100) quality = 100;
-                            normalizedOperations['quality'] = quality.toString();
-                        }
-                    }
-                    break;
-                default:
-                    break;
-            }
-        });
-        //rewrite the path to normalized version if valid operations are found
-        if (Object.keys(normalizedOperations).length > 0) {
-            // put them in order
-            var normalizedOperationsArray = [];
-            if (normalizedOperations.format) normalizedOperationsArray.push('format=' + normalizedOperations.format);
-            if (normalizedOperations.quality) normalizedOperationsArray.push('quality=' + normalizedOperations.quality);
-            if (normalizedOperations.width) normalizedOperationsArray.push('width=' + normalizedOperations.width);
-            if (normalizedOperations.height) normalizedOperationsArray.push('height=' + normalizedOperations.height);
-            request.uri = originalImagePath + '/' + normalizedOperationsArray.join(',');
-        } else {
-            // If no valid operation is found, flag the request with /original path suffix
-            request.uri = originalImagePath + '/original';
-        }
+const SUPPORTED_FORMATS = ['jpg', 'jpeg', 'png', 'webp', 'gif', 'tif', 'tiff', 'avif', 'svg'];
 
+function handler(event) {
+    const request = event.request;
+    const originalImagePath = request.uri;
+    // 验证、处理和规范化查询参数中的请求操作
+    const options = resolveOptions(request);
+    // 如果找到有效操作，则重写规范化版本的路径
+    if (options) {
+        // 按顺序重新组合
+        const optionArray = [];
+        if (options.format) optionArray.push('format=' + options.format);
+        if (options.quality) optionArray.push('quality=' + options.quality);
+        if (options.width) optionArray.push('width=' + options.width);
+        if (options.height) optionArray.push('height=' + options.height);
+        request.uri = originalImagePath + '/' + optionArray.join(',');
     } else {
-        // If no query strings are found, flag the request with /original path suffix
+        // 如果未找到有效参数，则使用 /original 路径后缀标记请求
         request.uri = originalImagePath + '/original';
     }
-    // remove query strings
-    request['querystring'] = {};
+
+    // 移除 ? 后的参数
+    request.querystring = {};
     return request;
+}
+
+// 解析参数
+function resolveOptions(request) {
+    const querystring = request.querystring;
+    if (!querystring) {
+        return {}
+    }
+
+    const options = {};
+    for (let optionKey of Object.keys(querystring)) {
+        // 无效 optionKey
+        if (!optionKey) {
+            continue;
+        }
+        // 无效 optionValue
+        let optionValue = querystring[optionKey].value;
+        if (!optionValue) {
+            continue
+        }
+        optionKey = optionKey.toLowerCase();
+        optionValue = optionValue.toLowerCase();
+        switch (optionKey) {
+            case 'format':
+                if (optionValue === 'auto') {
+                    const format = getFormatByAccept(request.headers.accept);
+                    if (format) {
+                        options['format'] = format;
+                    }
+                    break;
+                }
+                if (SUPPORTED_FORMATS.includes(optionValue)) {
+                    options['format'] = optionValue;
+                }
+                break;
+
+            case 'width':
+                const width = getOptionValue(optionValue, 4000);
+                if (width) {
+                    options['width'] = width;
+                }
+                break;
+
+            case 'height':
+                const height = getOptionValue(optionValue, 4000);
+                if (height) {
+                    options['height'] = height;
+                }
+                break;
+
+            case 'quality':
+                const quality = getOptionValue(optionValue, 100);
+                if (quality) {
+                    options['quality'] = quality;
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+}
+
+// 根据 accept 请求头自动判断格式
+function getFormatByAccept(acceptHeader) {
+    // header 不存在
+    if (!acceptHeader) {
+        return undefined;
+    }
+    // header 值不存在
+    let accept = acceptHeader.value;
+    if (!accept) {
+        return undefined;
+    }
+    // header 值小写
+    accept = accept.toLowerCase()
+    // 查找格式
+    for (let format of SUPPORTED_FORMATS) {
+        if (accept.includes(format)) {
+            return format;
+        }
+    }
+    return undefined;
+}
+
+// 将值转换为数字, 验证大小后, 返回字符串格式
+function getOptionValue(optionValue, maxValue) {
+    let value = parseInt(optionValue);
+    if (!isNaN(value) && (value > 0)) {
+        // you can protect the Lambda function by setting a max value;
+        if (value > maxValue) value = maxValue;
+        return value.toString();
+    }
+    return undefined;
 }
